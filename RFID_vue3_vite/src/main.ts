6d10287210ba3207347009876be480ed4607316d
wangyang-o <1370228031@qq.com>
2021-04-08 22:51:21 +0800

    封装cookie，axios

--- RFID_vue3_vite/src/main.ts
+++ RFID_vue3_vite/src/main.ts
@@ -3,14 +3,23 @@
  * @version: 
  * @Author: wy
  * @Date: 2021年04月07日 21:37:16
- * @LastEditors: wy
- * @LastEditTime: 2021年04月07日 22:23:17
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021年04月08日
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from "./router";
-import { store } from "./vuex";
+import { store, key } from "./vuex";
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-// 链式调用
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+// 注册相应模块
+app.use(ElementPlus)
+app.use(store, key)
+app.use(router)
+app.mount('#app')
+
+// 注册组件
+// app.component(ElButton.name, ElButton)
---
+++ RFID_vue3_vite/src/utils/cookie.ts
@@ -0,0 +1,46 @@
+/*
+ * @Author: wy
+ * @Date: 2021年04月08日
+ * @LastEditTime: 2021年04月08日
+ */
+import Cookies from 'js-cookie';
+// token
+const TokenKey = 'token';
+// 刷新token
+const RefreshTokenKey = 'refresh-token';
+// 获取 值 value
+export function getToken() {
+    return Cookies.get(TokenKey);
+}
+export function getRefreshToken() {
+    return Cookies.get(RefreshTokenKey)
+}
+// 获取 名 key
+export function getTokenKey() {
+    return TokenKey
+}
+export function getRefreshTokenKey() {
+    return RefreshTokenKey
+}
+// 设置cookie
+export function setToken(token: any) {
+    return Cookies.set(TokenKey, token)
+}
+export function setRefreshToken(refreshToken: any) {
+    return Cookies.set(RefreshTokenKey, refreshToken)
+}
+// 删除
+export function removeToken() {
+    Cookies.remove(TokenKey)
+    Cookies.remove(RefreshTokenKey)
+}
+const cookiesUtil = {
+    setToken,
+    setRefreshToken,
+    getToken,
+    getTokenKey,
+    getRefreshToken,
+    getRefreshTokenKey,
+    removeToken,
+}
+export default cookiesUtil;
