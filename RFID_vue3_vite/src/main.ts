71074d804209a1bcb982c2576a8b111edcefb065
wangyang-o <1370228031@qq.com>
2021-05-24 19:15:32 +0800

    按需导入elmentPlus

--- RFID_vue3_vite/src/main.ts
+++ RFID_vue3_vite/src/main.ts
@@ -4,14 +4,39 @@
  * @Author: wy
  * @Date: 2021年04月07日 21:37:16
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021年04月28日
+ * @LastEditTime: 2021年05月24日
  */
 import { createApp } from 'vue';
 import App from './App.vue';
 
 import router from './router';
 import store from './vuex';
-import ElementPlus from 'element-plus';
+// import ElementPlus from 'element-plus';
+import {
+	ElLoading,
+	ElMessage,
+	ElMessageBox,
+	ElNotification,
+	ElButton,
+	ElSelect,
+	ElCard,
+	ElCol,
+	ElRow,
+	ElTable,
+	ElTableColumn,
+	ElDrawer,
+	ElIcon,
+	ElImage,
+	ElInput,
+	ElInputNumber,
+	ElForm,
+	ElFormItem,
+	ElPopconfirm,
+	ElSkeleton,
+	ElPagination,
+	ElTag,
+	ElOption,
+} from 'element-plus';
 // 样式
 import 'element-plus/lib/theme-chalk/index.css';
 import 'animate.css/animate.min.css';
@@ -20,7 +45,7 @@ import '@/assets/css/common.css'; // 通用 css
 
 const app = createApp(App);
 // 注册相应模块
-app.use(ElementPlus);
+// app.use(ElementPlus);
 app.use(store);
 app.use(router);
 app.mount('#app');
@@ -28,3 +53,31 @@ app.mount('#app');
 // 全局注册组件（也可以使用局部注册）
 // app.component(ElButton.name, ElButton)
 // app.component('v-chart', ECharts);
+const components: any[] = [
+	ElButton,
+	ElSelect,
+	ElCard,
+	ElCol,
+	ElRow,
+	ElTable,
+	ElTableColumn,
+	ElDrawer,
+	ElIcon,
+	ElImage,
+	ElInput,
+	ElInputNumber,
+	ElForm,
+	ElFormItem,
+	ElPopconfirm,
+	ElSkeleton,
+	ElPagination,
+	ElOption,
+	ElTag,
+];
+components.forEach((component) => {
+	app.use(component);
+});
+const plugins = [ElLoading, ElMessageBox, ElNotification];
+plugins.forEach((plugin) => {
+	app.use(plugin);
+});
