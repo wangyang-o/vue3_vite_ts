78fd8694071ae6487d077719b63bcb81fc1e86b4
wangyang-o <1370228031@qq.com>
2021-05-24 21:16:52 +0800

    添加图片

--- RFID_vue3_vite/src/views/stockList/index.vue
+++ RFID_vue3_vite/src/views/stockList/index.vue
@@ -30,14 +30,6 @@
                 icon="el-icon-search"
               >搜索</el-button>
             </el-col>
-            <!-- <el-col :span="2">
-              <el-button
-                size="small"
-                type="warning"
-                @click="addOrUpdate('add')"
-                icon="el-icon-circle-plus"
-              >添加货物</el-button>
-            </el-col>-->
           </el-row>
         </el-form>
       </template>
@@ -46,8 +38,8 @@
   <!-- 表格 -->
   <el-card class="m-1">
     <el-carousel :interval="4000" type="card" height="200px">
-      <el-carousel-item v-for="item in 6" :key="item">
-        <img class="carousel-img" src="@/assets/img/404.jpg" />
+      <el-carousel-item v-for="(imgUrl,index) in imgList" :key="imgUrl">
+        <img class="carousel-img" :src="'src/assets/img/' + imgUrl" />
       </el-carousel-item>
     </el-carousel>
     <el-table size="small" :data="tableData" style="width: 100%" empty-text="没有数据了...">
@@ -59,24 +51,6 @@
       <el-table-column align="center" prop="createUser" label="创建人"></el-table-column>
       <el-table-column align="center" prop="updateTime" label="更新时间"></el-table-column>
       <el-table-column align="center" prop="updateUser" label="更新人"></el-table-column>
-      <!-- <el-table-column align="center" label="操作">
-        <template #default="scope">
-          <el-button size="mini" @click="handleEdit(scope.row.id)">编辑</el-button>
-          <el-popconfirm
-            confirmButtonText="确定"
-            cancelButtonText="点错了"
-            icon="el-icon-info"
-            iconColor="red"
-            :title="'确定删除 ' + scope.row.productName + ' 的所有信息吗？'"
-            trigger="click"
-            @confirm="handleDelete(scope.row.id)"
-          >
-            <template #reference>
-              <el-button size="mini" type="danger">删除</el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>-->
     </el-table>
     <el-pagination
       v-if="pagingHidden"
@@ -90,52 +64,6 @@
       style="text-align: center;"
     ></el-pagination>
   </el-card>
-  <!-- 添加 -->
-  <el-drawer
-    :title="addOrUpdateFlag === 'add' ? '添加货物' : '修改货物'"
-    :before-close="handleDrawerClose"
-    v-model="drawerIsOpen"
-    direction="rtl"
-  >
-    <div class="mx-5">
-      <el-form
-        ref="drawerFormRef"
-        :model="drawerForm"
-        status-icon
-        label-position="top"
-        :rules="rules"
-      >
-        <el-form-item label="姓名:" prop="productName">
-          <el-input v-model="drawerForm.productName" autocomplete="off" placeholder="请输入姓名"></el-input>
-        </el-form-item>
-        <el-form-item label="年龄:" prop="age">
-          <el-input v-model="drawerForm.age" autocomplete="off" placeholder="请输入年龄"></el-input>
-        </el-form-item>
-        <el-form-item label="电话:" prop="iphone">
-          <el-input v-model="drawerForm.iphone" autocomplete="off" placeholder="请输入电话"></el-input>
-        </el-form-item>
-        <el-form-item label="性别:" prop="sex">
-          <el-select v-model="drawerForm.sex" clearable style="width:100%" placeholder="请选择年龄">
-            <el-option label="男" value="男"></el-option>
-            <el-option label="女" value="女"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <el-row :gutter="10">
-        <el-col :span="12">
-          <el-button style="width:100%" @click="handleDrawerClose">取 消</el-button>
-        </el-col>
-        <el-col :span="12">
-          <el-button
-            style="width:100%"
-            type="primary"
-            @click="submitDrawer()"
-            :loading="loading"
-          >{{ loading ? '提交中 ...' : '确 定' }}</el-button>
-        </el-col>
-      </el-row>
-    </div>
-  </el-drawer>
 </template>
 <script lang="ts">
 import { ref, defineComponent, toRefs, reactive, onMounted } from 'vue';
@@ -165,6 +93,8 @@ export default defineComponent({
       size: 10,
       field: 'createTime',
     });
+    // imgList
+    const imgList = reactive(['store1.jpg', 'store2.jpg', 'store3.jpg', 'store4.jpg', 'store5.png']);
     // 列表数据
     const tableData = ref([]);
     // 列表数据总个数total
@@ -177,34 +107,7 @@ export default defineComponent({
       tableData.value = data.list;
       count.value = data.total;
     }
-    // 编辑功能
-    const handleEdit = async (productId: string) => {
-      addOrUpdate('edit');
-      const { code, data }: any = await getInstanceById(productId);
-      if (code === 0) {
-        drawerForm.value = data;
-      }
-    }
-    // 删除功能
-    const handleDelete = async (productId: number) => {
-      const { code, msg }: any = await deleteProduct({ ids: productId });
-      if (code === 0) {
-        searchproductName()
-        ElNotification({
-          title: '提示',
-          type: 'success',
-          message: msg,
-          duration: 1000,
-        });
-      } else {
-        ElNotification({
-          title: '错误',
-          type: 'error',
-          message: msg,
-          duration: 1000,
-        });
-      }
-    }
+
     // 根据姓名搜索
     const searchproductName = () => {
       queryParams.current = 1;
@@ -220,95 +123,12 @@ export default defineComponent({
       queryParams.current = val;
       productList();
     }
-    // Drawer相关
-    const drawerIsOpen = ref(false);
-    // 添加还是修改
-    const addOrUpdateFlag = ref('');
-    const addOrUpdate = (flag: string) => {
-      drawerIsOpen.value = !drawerIsOpen.value;
-      addOrUpdateFlag.value = flag;
-    }
-    // Drawer表单节点
-    const drawerFormRef = ref<HTMLElement | null>(null);
-    // Drawer表单数据
-    const drawerForm = ref({ productName: '', sex: '', age: '', iphone: '' });
-    // 验证规则
-    const rules = {
-      productName: [
-        { required: true, message: "请输入货物姓名", trigger: "blur" },
-        {
-          min: 2,
-          max: 30,
-          message: "长度在 3 到 30 个字符",
-          trigger: "blur",
-        },
-      ],
-      sex: { required: true, message: "请选择性别", trigger: "blur" },
-      age: { required: true, message: "请输入年龄", trigger: "blur" },
-      iphone: { max: 20, required: true, message: "请输入电话", trigger: "blur" },
 
-    }
-    // 关闭drawer
-    const handleDrawerClose = () => {
-      (drawerFormRef.value as any).resetFields();
-      loading.value = false;
-      drawerIsOpen.value = false;
-      delete (drawerForm.value as any).id;
-    }
-    const submitDrawer = () => {
-      (drawerFormRef.value as any).validate(async (valid: boolean) => {
-        if (!valid) return;
-        loading.value = true;
-        if (addOrUpdateFlag.value === 'add') {
-          const { code, msg }: any = await putProduct(drawerForm.value);
-          if (code === 0) {
-            handleDrawerClose();
-            searchproductName();
-            ElNotification({
-              type: 'success',
-              message: msg,
-              duration: 1000, position: 'bottom-right'
-            });
-          } else {
-            ElNotification({
-              type: 'error',
-              message: msg,
-              duration: 1000, position: 'bottom-right'
-            });
-          }
-        } else {
-
-          const { code, msg }: any = await putProduct(drawerForm.value);
-          console.log('edit');
-          if (code === 0) {
-            handleDrawerClose();
-            searchproductName();
-            ElNotification({
-              type: 'success',
-              message: msg,
-              duration: 1000, position: 'bottom-right'
-            });
-          } else {
-            ElNotification({
-              type: 'error',
-              message: msg,
-              duration: 1000, position: 'bottom-right'
-            });
-          }
-        }
-
-
-      })
-
-    }
     // 初始化table
     productList();
     return {
-      animateFlag, ...toRefs(queryParams), tableData, count, loading,
-      searchproductName, pagingHidden,
-      drawerForm, handleDrawerClose, drawerIsOpen, submitDrawer, rules, drawerFormRef,
-      addOrUpdate, addOrUpdateFlag,
-      handleEdit, handleDelete, handleCurrentChange, indexMethod
+      animateFlag, ...toRefs(queryParams), tableData, count, loading, imgList,
+      searchproductName, pagingHidden, handleCurrentChange, indexMethod
     };
   },
 });
