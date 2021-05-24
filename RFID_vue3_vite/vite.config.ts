e5ee5b03eab1776a8cd8b2bb71e29dfff6206b70
wangyang-o <1370228031@qq.com>
2021-05-24 19:23:05 +0800

    修改vite配置

--- RFID_vue3_vite/vite.config.ts
+++ RFID_vue3_vite/vite.config.ts
@@ -4,7 +4,7 @@
  * @Author: wy
  * @Date: 2021年04月07日 21:37:16
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021年05月18日
+ * @LastEditTime: 2021年05月24日
  */
 import { defineConfig } from 'vite';
 // Vue 3 单文件组件支持
@@ -39,6 +39,26 @@ export default defineConfig({
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			},
 		},
+	},
+	build: {
+		// 设置最终构建的浏览器兼容目标
+		target: 'es2015',
+
+		terserOptions: {
+			// 压缩
+			compress: {
+				// https://terser.org/docs/api-reference#compress-options
+				// Pass true to prevent Infinity from being compressed into 1/0, which may cause performance issues on Chrome.
+				keep_infinity: true,
+				// Used to delete console in production environment
+				drop_console: true,
+			},
+		},
+		// Turning off brotliSize display can slightly reduce packaging time
+		// 关闭brotliSize显示可以稍微缩短打包时间
+		brotliSize: false,
+		// 超过多少警告
+		chunkSizeWarningLimit: 1500,
 	},
 	// 插件
 	plugins: [vue()],
